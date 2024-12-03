ec67cd7817e481ffbbb06edc1625eac3ce971866
Kacian417 <knicho4492@gmail.com>
2024-12-03 02:30:31 +0000

    started project

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, Vehicle, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,84 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+#GET all characters (people) and specific characters via routes
+@app.route("/characters", methods=["GET"])
+def get_all_characters():
+    #query the database to get all the starwars characters
+    all_characters = Character.query.all()
+
+    if all_characters is None:
+        return jsonify("No records found."), 404
+    else:
+        all_characters = list(map(lambda x: x.serialize(), all_characters))
+        return jsonify(all_characters), 200
+
+
+
+@app.route("/characters/<int:character_id>", methods=["GET"])
+def get_character(character_id):
+    single_character = Character.query.get(character_id)
+
+    if single_character is None:
+        raise APIException(f'Character ID {character_id} is not found!', status_code=404)
+    
+    single_character = single_character.serialize()
+    return jsonify(single_character), 200
+
+#Planets
+@app.route("/planets", methods=["GET"])
+def get_all_planets():
+    pass
+
+
+#GET single planet
+
+
+#GET all vehicles
+
+
+#GET single vehicle
+
+
+#GET all users
+
+
+#GET one user's favorites
+
+
+
+#POST favorite planet
+
+
+#POST favorite character
+@app.route("/favorite/characters", methods=["POST"])
+def add_favorite_person():
+
+
+    #retrieve info found in the body portion of the client request
+    data = request.get_json()
+    new_favorite_character = Favorites(user_id = data["user_id"], character_id = data["character_id"])
+    db.session.add(new_favorite_character)
+    db.session.commit()
+
+    return jsonify("Your favortie was added."), 200
+    
+
+
+#POST favorite vehicle
+
+
+
+#DELETE favorite planet
+
+
+#DELETE favorite character
+
+
+#DELETE favorite vehicle
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,133 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+#class User(db.Model):
+    #id = db.Column(db.Integer, primary_key=True)
+    #email = db.Column(db.String(120), unique=True, nullable=False)
+    #password = db.Column(db.String(80), unique=False, nullable=False)
+    #is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    #def __repr__(self):
+        #return '<User %r>' % self.username
+
+    #def serialize(self):
+        #return {
+            #"id": self.id,
+            #"email": self.email,
+            # do not serialize the password, its a security breach
+        #}
+    
+
 class User(db.Model):
+    __tablename__ = 'users'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String, unique=False, nullable=False)
+    first_name = db.Column(db.String, unique=False, nullable=False)
+    last_name = db.Column(db.String, unique=False, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    phone_number = db.Column(db.String, unique=False, nullable=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
 
     def serialize(self):
         return {
-            "id": self.id,
+            "id":self.id,
+            "username": self.username,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "phone_number": self.phone_number,
+            }
+    
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    planet_name = db.Column(db.String, unique=False, nullable=False)
+    population = db.Column(db.String, unique=False, nullable=False)
+    climate = db.Column(db.String, unique=False, nullable=False)
+    #films = db.Column(db.String, unique=False, nullable=False)
+    #user_id = db.Column(db.Integer, ForeignKey('users.id'))
+    #user = relationship(User)
+
+    def to_dict(self):
+        return {}
+    
+    def serialize(self):
+        return {
+            "id":self.id,
+            "planet_name": self.planet_name,
+            "population": self.population,
+            "climate": self.climate,
+            }
+    
+class Character(db.Model):
+    __tablename__ = 'characters'
+
+    id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String, unique=False, nullable=False)
+    birth_year = db.Column(db.String, unique=False, nullable=False)
+    gender = db.Column(db.String, unique=False, nullable=False)
+    #homeworld = db.Column(db.String, unique=False, nullable=False)
+    #films = db.Column(db.String, unique=False, nullable=False)
+    #user_id = db.Column(db.Integer, ForeignKey('users.id'))
+    #user = relationship(User)
+
+    def to_dict(self):
+        return {}  
+    def serialize(self):
+        return {
+            "id":self.id,
+            "character_name": self.character_name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            }
+
+class Vehicle(db.Model):
+    __tablename__ = 'vehicles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    vehicle_name = db.Column(db.String, unique=False, nullable=False)
+    model = db.Column(db.String, unique=False, nullable=False)
+    #length = db.Column(db.String, unique=False, nullable=False)
+    #crew = db.Column(db.String, unique=False, nullable=False)
+    #films = db.Column(db.String, unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship(User)
+
+    def to_dict(self):
+        return {}  
+    def serialize(self):
+        return {
+            "id":self.id,
+            "name": self.name,
+            "model": self.model,
+            } 
+    
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship(User)
+
+    character_id = db.Column(db.Integer, db.ForeignKey('characters.id'), nullable=True)
+    character = db.relationship(Character)
+
+    #planet_id = db.Column(db.Integer, db.ForeignKey('Planet.id'), nullable=True)
+    #planet = db.relationship(Planet)
+
+    #vehicle_id = db.Column(db.Integer, db.ForeignKey('Vehicle.id'), nullable=True)
+    #vehicle = db.relationship(Vehicle)
+
+    def serialize(self):
+        return {
+            "id": self.user_id,
+            "character_id": self.character_id,
+            #"planet_id": self.planet_id,
+            #"vehicle_id": self.vehicle_id,
         }
